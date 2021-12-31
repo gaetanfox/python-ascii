12df333a5c969d0d732c4739f4b5439c29c67d97
Cody McCodePants <cody@mccodepants.com>
2021-12-31 14:10:18 +0300

    adding ascii gen

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 import glob
-import shutil
 import os
+import shutil
 from PIL import Image, ImageOps, ImageEnhance, ImageDraw, ImageFont
 
 
@@ -8,12 +8,6 @@ class Digitizer:
     def __init__(self, filepath):
         self.filepath = filepath
         self.img = Image.open(filepath).convert("RGBA")
-
-    def save(self, output_filepath):
-        print("This has saved")
-        if self.filepath.endswith(".jpg"):
-            self.img = self.img.convert("RGB")
-        self.img.save(output_filepath)
 
     def adjust_contrast(self, amount=1.5):
         enhancer = ImageEnhance.Contrast(self.img)
@@ -23,43 +17,89 @@ class Digitizer:
         self.img = ImageOps.grayscale(self.img)
         self.img = self.img.convert("RGBA")
 
-    def make_upside_down(self):
-        self.img = self.img.rotate(180)
-
     def make_thumbnail_size(self, size=(128, 128)):
         self.img.thumbnail(size)
 
     def make_square(self, size=200):
+        print("make it a square")
+
         (w, h) = self.img.size
+
+        # if portrait
         if w > h:
             x = (w - h) * 0.5
             y = 0
-            box = (x, y, h + x, y + h)
+            box = (x, y, h + x, h + y)
         else:
             x = 0
             y = (h - w) * 0.5
             box = (x, y, x + w, y + w)
+
         self.img = self.img.resize((size, size), box=box)
 
     def add_watermark(self):
-        font = ImageFont.truetype("ibm-plex-mono.ttf", 16)
+        font = ImageFont.truetype("ibm-plex-mono.ttf", 24)
+
         drawer = ImageDraw.Draw(self.img)
 
-        drawer.text((32, 32), "Fox watermark", font=font, fill=(255, 0, 0, 100))
+        drawer.multiline_text(
+            (32, 32), "SuperHi\nwatermark", font=font, fill=(255, 0, 0, 100)
+        )
+
+    def convert_to_ascii(self):
+        font_size = 10
+
+        letters = [" ", ".", "!", "&", "@"]
+
+        (w, h) = self.img.size
+
+        new_width = int(w / font_size)
+        new_height = int(h / font_size)
+
+        sample_size = (new_width, new_height)
+        final_size = (new_width * font_size, new_height * font_size)
+
+        self.make_grayscale()
+
+        self.adjust_contrast(5.0)
+
+        self.img = self.img.resize(sample_size)
+
+        ascii_img = Image.new("RGBA", final_size, color="#2727e6")
+
+        font = ImageFont.truetype("ibm-plex-mono.ttf", font_size)
+        drawer = ImageDraw.Draw(ascii_img)
+
+        for x in range(new_width):
+            for y in range(new_height):
+                (r, g, b, a) = self.img.getpixel((x, y))
+
+                brightness = r / 256
+
+                letter_num = int(len(letters) * brightness)
+                letter = letters[letter_num]
+
+                position = (x * font_size, y * font_size)
+                drawer.text(position, letter, font=font, fill=(255, 255, 255, 255))
+
+        self.img = ascii_img
+
+    def save(self, output_filepath):
+        print("This has saved!!!!")
+        if self.filepath.endswith(".jpg"):
+            self.img = self.img.convert("RGB")
+
+        self.img.save(output_filepath)
 
 
-inputs = glob.glob("inputs/*.jpg")
+if __name__ == "__main__":
+    inputs = glob.glob("inputs/*")
 
-os.makedirs("outputs", exist_ok=True)
+    os.makedirs("outputs", exist_ok=True)
 
-for filepath in inputs:
-    output = filepath.replace("inputs", "outputs")
+    for filepath in inputs:
+        output = filepath.replace("inputs", "outputs")
 
-    image = Digitizer(filepath)
-    # image.make_upside_down()
-    # image.make_thumbnail_size((200, 200))
-    image.make_square()
-    image.make_grayscale()
-    image.adjust_contrast()
-    image.add_watermark()
-    image.save(output)
+        image = Digitizer(filepath)
+        image.convert_to_ascii()
+        image.save(output)
